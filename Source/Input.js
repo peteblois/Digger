a2505f9210172980591768ded4a211e8a2fc083f
Lutz Roeder <lutzroeder@users.noreply.github.com>
2011-01-02 04:16:25 -0800

    fix firefox keypress handling

--- Source/Input.js
+++ Source/Input.js
@@ -9,24 +9,33 @@ function Input(canvas, game)
 	this.touchEndHandler = this.touchEnd.delegate(this);
 	this.touchMoveHandler = this.touchMove.delegate(this);
 	this.keyDownHandler = this.keyDown.delegate(this);
+	this.keyPressHandler = this.keyPress.delegate(this);
 	this.keyUpHandler = this.keyUp.delegate(this);
 	this.canvas.addEventListener("touchstart", this.touchStartHandler, false);
 	this.canvas.addEventListener("touchmove", this.touchMoveHandler, false);
 	this.canvas.addEventListener("touchend", this.touchEndHandler, false);
 	this.canvas.addEventListener("mousedown", this.mouseDownHandler, false);
 	document.addEventListener("keydown", this.keyDownHandler, false);
+	document.addEventListener("keypress", this.keyPressHandler, false);
 	document.addEventListener("keyup", this.keyUpHandler, false);
+	this.isWebKit = typeof navigator.userAgent.split("WebKit/")[1] !== "undefined";
+	this.isMozilla = navigator.appVersion.indexOf('Gecko/') >= 0 || ((navigator.userAgent.indexOf("Gecko") >= 0) && !this.isWebKit && (typeof navigator.appVersion !== "undefined"));
 }
 
 Input.prototype.keyDown = function(e)
 {
-	     if (e.keyCode == 37) { e.preventDefault(); this.game.addKey(Key.left);  } // left
-	else if (e.keyCode == 39) { e.preventDefault(); this.game.addKey(Key.right); } // right
-	else if (e.keyCode == 38) { e.preventDefault(); this.game.addKey(Key.up);    } // up
-	else if (e.keyCode == 40) { e.preventDefault(); this.game.addKey(Key.down);  } // down
-	else if (e.keyCode == 27) { e.preventDefault(); this.game.addKey(Key.reset); } // esc
-	else if (e.keyCode == 32) { e.preventDefault(); this.game.nextLevel();       } // space
-	else if (!this.game.isAlive()) { e.preventDefault(); this.game.addKey(Key.reset); }
+	if (!this.isMozilla)
+	{
+		this.processKey(e, e.keyCode);
+	}
+};
+
+Input.prototype.keyPress = function(e)
+{
+	if (this.isMozilla)
+	{
+		this.processKey(e, (e.keyCode != 0) ? e.keyCode : (e.charCode === 32) ? 32 : 0);
+	}
 };
 
 Input.prototype.keyUp = function(e)
@@ -35,6 +44,17 @@ Input.prototype.keyUp = function(e)
 	else if (e.keyCode == 39) { this.game.removeKey(Key.right); }
 	else if (e.keyCode == 38) { this.game.removeKey(Key.up);    }
 	else if (e.keyCode == 40) { this.game.removeKey(Key.down);  }
+};
+
+Input.prototype.processKey = function(e, keyCode)
+{
+	     if (keyCode == 37) { this.stopEvent(e); this.game.addKey(Key.left);  } // left
+	else if (keyCode == 39) { this.stopEvent(e); this.game.addKey(Key.right); } // right
+	else if (keyCode == 38) { this.stopEvent(e); this.game.addKey(Key.up);    } // up
+	else if (keyCode == 40) { this.stopEvent(e); this.game.addKey(Key.down);  } // down
+	else if (keyCode == 27) { this.stopEvent(e); this.game.addKey(Key.reset); } // esc
+	else if (keyCode == 32) { this.stopEvent(e); this.game.nextLevel();       } // space
+	else if (!this.game.isAlive()) { this.stopEvent(e); this.game.addKey(Key.reset); }
 };
 
 Input.prototype.mouseDown = function(e) 
@@ -116,4 +136,10 @@ Input.prototype.touchEnd = function(e)
 	this.game.removeKey(Key.right);
 	this.game.removeKey(Key.up);
 	this.game.removeKey(Key.down);
+};
+
+Input.prototype.stopEvent = function(e)
+{
+	e.preventDefault();
+	e.stopPropagation();
 };
