ab77289896ecc6d21220915fa313961a0dbcac04
Lutz Roeder <lutzroeder@users.noreply.github.com>
2012-10-01 10:11:39 -0700

    Update to use TypeScript

--- Tools/build.sh
+++ Tools/build.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
-rm -r ../Build
-mkdir ../Build
-mkdir ../Build/Debug
-mkdir ../Build/Release
-cd ../Source
-cp Default.html ../Build/Debug/default.html
-cp Default.html ../Build/Release/default.html
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+rm -f -r ../Build
+mkdir -p ../Build/Debug
+mkdir -p ../Build/Release
+
+cp ../Source/Default.html ../Build/Debug/default.html
+cp ../Source/Default.html ../Build/Release/default.html
+
 echo Building \'Debug/digger.js\'
-target=../Build/Debug/digger.js
-cat	Direction.js \
-	Sprite.js \
-	Key.js \
-	Sound.js \
-	Player.js \
-	Ghost.js \
-	Position.js \
-	Base64Reader.js \
-	Level.js \
-	Display.js \
-	Loader.js \
-	Input.js \
-	Function.js \
-	Digger.js \
-	LevelData.js \
-	> $target
-echo 'Digger.prototype.imageData = [ "'$(openssl base64 -in Sprite.png | tr -d '\n')'","'$(openssl base64 -in Font.png | tr -d '\n')'" ];' >> $target  
-echo 'Digger.prototype.soundData = [ "'$(openssl base64 -in Diamond.wav | tr -d '\n')'","'$(openssl base64 -in Stone.wav | tr -d '\n')'","'$(openssl base64 -in Step.wav | tr -d '\n')'" ];' >> $target  
+
+node tsc.js -target ES5 -out ../Build/Debug/digger.js lib.d.ts libex.ts ../Source/*.ts
+
+node base64.js ../Build/Debug/digger.js Digger.Game.prototype.imageData ../Source/Sprite.png ../Source/Font.png
+node base64.js ../Build/Debug/digger.js Digger.Game.prototype.soundData ../Source/Diamond.wav ../Source/Stone.wav ../Source/Step.wav
+
 echo Building \'Release/digger.js\'
-cd ../Tools
-java -jar compiler.jar --js ../Build/Debug/digger.js > ../Build/Release/digger.js
+
+node minify.js ../Build/Debug/digger.js ../Build/Release/digger.js
+
 echo Done.
