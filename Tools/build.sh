8d7e26a1b80f6fc6f39f2c6efc2a0025d7a61b84
Lutz Roeder <lutzroeder@users.noreply.github.com>
2013-02-21 15:34:44 -0800

    Updated libex.ts file name

--- Tools/build.sh
+++ Tools/build.sh
@@ -11,7 +11,7 @@ cp ../Source/Default.html ../Build/Release/default.html
 
 echo Building \'Debug/digger.js\'
 
-node tsc.js -target ES5 -out ../Build/Debug/digger.js lib.d.ts libex.ts ../Source/*.ts
+node tsc.js -target ES5 -out ../Build/Debug/digger.js lib.d.ts libex.d.ts ../Source/*.ts
 
 node base64.js ../Build/Debug/digger.js Digger.Game.prototype.imageData ../Source/Sprite.png ../Source/Font.png
 node base64.js ../Build/Debug/digger.js Digger.Game.prototype.soundData ../Source/Diamond.wav ../Source/Stone.wav ../Source/Step.wav
