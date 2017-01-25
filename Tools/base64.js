37d6c8912f4dc7fd0effcd94e5eb896da041d406
Lutz Roeder <lutzroeder@users.noreply.github.com>
2017-01-25 13:04:50 -0800

    Install TypeScript package

--- Tools/base64.js
+++ Tools/base64.js
@@ -5,12 +5,12 @@ var arguments = process.argv.splice(2);
 
 if (arguments.length < 3)
 {
-	console.log("JavaScript Base64 Resource Converter, Version 1.0");
- 	console.log("Converts binary files to JavaScript base64 array and appends to output file.");
- 	console.log();
-	console.log("Usage: node base64.js <output.js> <javascript-variable> <file1> <file2> ...");
- 	console.log();
-	return;
+    console.log("JavaScript Base64 Resource Converter, Version 1.0");
+    console.log("Converts binary files to JavaScript base64 array and appends to output file.");
+    console.log();
+    console.log("Usage: node base64.js <output.js> <javascript-variable> <file1> <file2> ...");
+    console.log();
+    return;
 }
 
 var targetPath = arguments[0];
@@ -22,14 +22,14 @@ fs.writeSync(targetFile, " = [ ", null, "utf-8");
 
 for (var i = 2; i < arguments.length; i++)
 {
-	if (i !== 2)
-	{
-		fs.writeSync(targetFile, ", ", null, "utf-8");
-	}
+    if (i !== 2)
+    {
+        fs.writeSync(targetFile, ", ", null, "utf-8");
+    }
 
-	var data = fs.readFileSync(arguments[i]);
-	var text = data.toString("base64");
-	fs.writeSync(targetFile, '"' + text + '"', null, "utf-8");
+    var data = fs.readFileSync(arguments[i]);
+    var text = data.toString("base64");
+    fs.writeSync(targetFile, '"' + text + '"', null, "utf-8");
 }
 
 fs.writeSync(targetFile, " ];\n\r", null, "utf-8");
