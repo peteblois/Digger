8e92c7fad40d6574c22f5476dd3528805d312e5a
Lutz Roeder <lutzroeder@users.noreply.github.com>
2017-02-04 18:49:29 -0800

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp");
+var transform = require('gulp-transform');
+var rename = require('gulp-rename');
+var merge = require('gulp-merge');
+var sourcemaps = require('gulp-sourcemaps');
+var typescript = require('gulp-typescript');
+var uglify = require('gulp-uglify');
+
+gulp.task("default", function() {
+    var sources = gulp.src("./src/*.ts");
+    var resources = gulp.src([ "./src/*.gif", "./src/*.wav" ])
+        .pipe(transform(function(contents, file) { return "window['" + file.relative + "'] = '" + contents.toString("base64") + "';"; }))
+        .pipe(rename(function(path) { path.extname += ".ts"; }));
+    var compile = merge(sources, resources)
+        .pipe(sourcemaps.init())
+        .pipe(typescript({ target: "ES5", out: "digger.js" }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("./dist"));
+    var copy = gulp.src([ "./src/index.html" ])
+        .pipe(gulp.dest("./dist"))
+    return [ compile, copy ];
+});
